ad071e13a6175556af9d811b55cff61ed4644596
Ayush Garg <elliott@Ayush-MacBook-Air.local>
2024-09-11 18:46:49 +0530

    back to tokyonight colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,4 @@
-vim.g.nord_contrast = true
-vim.g.nord_borders = false
-vim.g.nord_disable_background = false
-vim.g.nord_italic = false
-vim.g.nord_uniform_diff_background = true
-vim.g.nord_bold = false
-
-local colorscheme = "nord"
+local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
