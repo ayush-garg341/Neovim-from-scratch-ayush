e85f4c663e9ffa45d7ba5324fdb30b88a7aeabe5
Ayush Garg <gargayush341@gmail.com>
2024-09-19 17:48:24 +0530

    added venv path to resolve imports

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -1,6 +1,8 @@
 return {
   settings = {
     python = {
+      venvPath = "/Users/elliott/projects/data-science/venv",
+      venv = "venv",
       analysis = {
         typeCheckingMode = "on",
       },
