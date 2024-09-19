ffe515513dc055bf1dd913defff6309ba182feed
Ayush Garg <gargayush341@gmail.com>
2024-09-19 18:44:52 +0530

    Revert "added venv path to resolve imports"
    This reverts commit e85f4c663e9ffa45d7ba5324fdb30b88a7aeabe5.

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -1,8 +1,6 @@
 return {
   settings = {
     python = {
-      venvPath = "/Users/elliott/projects/data-science/venv",
-      venv = "venv",
       analysis = {
         typeCheckingMode = "on",
       },
