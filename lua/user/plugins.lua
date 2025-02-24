69926d9bf4ff2fb6358899eaf705584ffcda2f5e
Ayush Garg <gargayush341@gmail.com>
2025-02-24 11:01:19 +0530

    markdown renderer and markdown preview installed

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -100,6 +100,24 @@ return packer.startup(function(use)
   -- Orgmode for note taking
   use {'nvim-orgmode/orgmode', config = function() require('orgmode').setup{} end}
 
+  -- Markdown renderer
+  use({
+      'MeanderingProgrammer/render-markdown.nvim',
+      after = { 'nvim-treesitter' },
+      requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+      -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+      requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+      config = function()
+          require('render-markdown').setup({})
+      end,
+  })
+
+  -- Markdown Preview
+  use({
+      "iamcco/markdown-preview.nvim",
+      run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
