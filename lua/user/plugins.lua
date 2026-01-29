84add977cb346060ca2ca3671e4b673bc354f74a
Ayush Garg <gargayush341@gmail.com>
2026-01-29 16:25:02 +0530

    nord theme updated

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -59,8 +59,8 @@ return packer.startup(function(use)
 
 	-- Colorschemes
   -- use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
-  use {'morhetz/gruvbox'}
-  -- use 'shaunsingh/nord.nvim'
+  -- use {'morhetz/gruvbox'}
+  use 'shaunsingh/nord.nvim'
   use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
 
 	-- Cmp 
